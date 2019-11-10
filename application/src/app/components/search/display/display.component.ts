7c11c08f45fcb618b6b13ecdda27040ae96ada02
Abhijith <vuduab01@pfw.edu>
2019-11-10 18:46:57 -0500

    updates

--- application/src/app/components/search/display/display.component.ts
+++ application/src/app/components/search/display/display.component.ts
@@ -7,12 +7,28 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
   @Input() restaurantData: any;
+  visible: boolean[];
+  save: boolean[];
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit() {
-
+    this.visible = new Array(this.restaurantData.length).fill(true);
+    this.save = new Array(this.restaurantData.length).fill(true);
   }
 
+  saveRestaurant(restaurant, index) {
+    console.log(restaurant);
+    this.visible = this.visible.map((value, i) => {
+      if (i === index) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    this.save[index] = this.visible[index];
+  }
 
 }
